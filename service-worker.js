40d9fd791e0a082bd9ac9cd874ba234b45fb06e4
ViPiven <vitalijjpiven777@gmail.com>
2021-05-28 13:30:04 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.44c5a438f63477e3c2523e933af0ee7e.js"
 );
 
 self.addEventListener('message', (event) => {
